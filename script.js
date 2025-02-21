89170bb9ce9368305b4dfb33c8ac169831217e8a
vedikathote <vedikathote@gmail.com>
2025-02-21 12:27:34 +0530

    made responsive navbar

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-let navbar=document.getElementById("vedi");
-navbar.addEventListener("click", function () {
-    alert("Button clicked!");
+document.addEventListener("DOMContentLoaded", function () {
+    let menu = document.querySelector(".links");
+    let menuIcon = document.querySelector(".fa-bars");
+    let closeIcon = document.querySelector(".fa-xmark");
+   
+    
+    menuIcon.addEventListener("click", function () {
+      menu.classList.add("links2");
+      menuIcon.style.display = "none"; // Hide bars icon
+      closeIcon.style.display = "block"; // Show close icon
+    });
+  
+    closeIcon.addEventListener("click", function () {
+      menu.classList.remove("links2");
+      menuIcon.style.display = "block"; // Show bars icon
+      closeIcon.style.display = "none"; // Hide close icon
+    });
+
+   
   });
+  
